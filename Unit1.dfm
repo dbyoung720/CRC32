6574ece9216623b4b6c6a55ca6d830552b0944f4
dbyoung720 <dbyoung@sina.com>
2021-08-05 08:51:55 +0800

    add test code

--- Unit1.dfm
+++ Unit1.dfm
@@ -1,7 +1,7 @@
 object Form1: TForm1
   Left = 0
   Top = 0
-  Caption = 'Form1'
+  Caption = 'CRC32 '#26657#39564#65306
   ClientHeight = 299
   ClientWidth = 635
   Color = clBtnFace
@@ -12,17 +12,42 @@ object Form1: TForm1
   Font.Style = []
   OldCreateOrder = False
   Position = poScreenCenter
-  OnCreate = FormCreate
   DesignSize = (
     635
     299)
   PixelsPerInch = 96
   TextHeight = 13
+  object lbl1: TLabel
+    Left = 21
+    Top = 12
+    Width = 90
+    Height = 15
+    Caption = #23383#31526#20018#26657#39564#65306
+    Font.Charset = GB2312_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -15
+    Font.Name = #23435#20307
+    Font.Style = []
+    ParentFont = False
+  end
+  object lbl2: TLabel
+    Left = 36
+    Top = 41
+    Width = 75
+    Height = 15
+    Caption = #25991#20214#26657#39564#65306
+    Font.Charset = GB2312_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -15
+    Font.Name = #23435#20307
+    Font.Style = []
+    ParentFont = False
+  end
   object mmoLog: TMemo
     Left = 8
-    Top = 8
+    Top = 72
     Width = 619
-    Height = 283
+    Height = 219
     Anchors = [akLeft, akTop, akRight, akBottom]
     Font.Charset = GB2312_CHARSET
     Font.Color = clWindowText
@@ -31,5 +56,61 @@ object Form1: TForm1
     Font.Style = []
     ParentFont = False
     TabOrder = 0
+  end
+  object edt1: TEdit
+    Left = 108
+    Top = 12
+    Width = 445
+    Height = 23
+    Anchors = [akLeft, akTop, akRight]
+    Font.Charset = GB2312_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -15
+    Font.Name = #23435#20307
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 1
+    Text = '1234567890'
+  end
+  object srchbxFile: TSearchBox
+    Left = 108
+    Top = 39
+    Width = 445
+    Height = 23
+    Anchors = [akLeft, akTop, akRight]
+    Font.Charset = GB2312_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -15
+    Font.Name = #23435#20307
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 2
+    Text = 'C:\Windows\notepad.exe'
+    OnInvokeSearch = srchbxFileInvokeSearch
+  end
+  object btnBuffer: TButton
+    Left = 564
+    Top = 10
+    Width = 63
+    Height = 25
+    Anchors = [akTop, akRight]
+    Caption = 'calc'
+    TabOrder = 3
+    OnClick = btnBufferClick
+  end
+  object btnFile: TButton
+    Left = 564
+    Top = 38
+    Width = 63
+    Height = 25
+    Anchors = [akTop, akRight]
+    Caption = 'calc'
+    TabOrder = 4
+    OnClick = btnFileClick
+  end
+  object dlgOpen1: TOpenDialog
+    Filter = '*.*|*.*'
+    Left = 156
+    Top = 132
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -3,12 +3,21 @@ unit Unit1;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, System.Diagnostics;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, System.Diagnostics, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.WinXCtrls;
 
 type
   TForm1 = class(TForm)
     mmoLog: TMemo;
-    procedure FormCreate(Sender: TObject);
+    lbl1: TLabel;
+    lbl2: TLabel;
+    edt1: TEdit;
+    srchbxFile: TSearchBox;
+    btnBuffer: TButton;
+    btnFile: TButton;
+    dlgOpen1: TOpenDialog;
+    procedure btnBufferClick(Sender: TObject);
+    procedure btnFileClick(Sender: TObject);
+    procedure srchbxFileInvokeSearch(Sender: TObject);
   private
     { Private declarations }
   public
@@ -22,25 +31,56 @@ implementation
 
 {$R *.dfm}
 
-uses db.CRC32;
+uses db.CRC32, System.ZLib, IdHashCRC;
 
-procedure TForm1.FormCreate(Sender: TObject);
+{ Indy CRC32 String }
+function TestIndyCRC32(const strValue: String): DWORD;
 var
-  strTemp    : String;
-  strFileName: String;
-  Buffer     : TBytes;
+  MyCRC: TIdHashCRC32;
 begin
-  strTemp := '1234567890';
-  SetLength(Buffer, Length(strTemp));
-  Buffer := BytesOf(strTemp);
-  mmoLog.Lines.Add('CRC32I Table: $' + IntToHex(CRC32I_Table(@Buffer[0], Length(strTemp)), 8));
-  mmoLog.Lines.Add('CRC32C Table: $' + IntToHex(CRC32C_Table(@Buffer[0], Length(strTemp)), 8));
-  mmoLog.Lines.Add('CRC32C SSE42: $' + IntToHex(CRC32C_SSE42(@Buffer[0], Length(strTemp)), 8));
+  MyCRC  := TIdHashCRC32.Create;
+  Result := MyCRC.HashValue(strValue);
+  MyCRC.Free;
+end;
 
-  // strFileName := 'F:\Backup\OS\WIN10\2021.iso';
-  // strFileName := 'C:\Windows\notepad.exe';
-  strFileName := 'F:\Download\ArrayFire-v3.8.0-CUDA-11.2.exe';
+{ zlib CRC32 String }
+function TestZlibCRC32(const strValue: String): DWORD;
+var
+  Buffer: TBytes;
+begin
+  SetLength(Buffer, Length(strValue));
+  Buffer := BytesOf(strValue);
+  Result := CRC32(0, @Buffer[0], Length(strValue));
+end;
 
+procedure TForm1.btnBufferClick(Sender: TObject);
+var
+  strTemp: String;
+  Buffer : TBytes;
+begin
+  if Trim(edt1.Text) <> '' then
+  begin
+    mmoLog.Clear;
+    strTemp := edt1.Text;
+    SetLength(Buffer, Length(strTemp));
+    Buffer := BytesOf(strTemp);
+    mmoLog.Lines.Add('CRC32I Table: $' + IntToHex(CRC32I_Table(@Buffer[0], Length(strTemp)), 8));
+    mmoLog.Lines.Add('CRC32I Indy : $' + IntToHex(TestIndyCRC32(strTemp), 8));
+    mmoLog.Lines.Add('CRC32I zlib : $' + IntToHex(TestZlibCRC32(strTemp), 8));
+    mmoLog.Lines.Add('CRC32C Table: $' + IntToHex(CRC32C_Table(@Buffer[0], Length(strTemp)), 8));
+    mmoLog.Lines.Add('CRC32C SSE42: $' + IntToHex(CRC32C_SSE42(@Buffer[0], Length(strTemp)), 8));
+  end;
+end;
+
+procedure TForm1.btnFileClick(Sender: TObject);
+var
+  strFileName: String;
+begin
+  if not FileExists(srchbxFile.Text) then
+    Exit;
+
+  mmoLog.Clear;
+  strFileName := srchbxFile.Text;
   with TStopwatch.StartNew do
   begin
     mmoLog.Lines.Add(Format('CRC32I Table_File: $%x；用时：%.5d 毫秒', [CRC32I_Table_File(strFileName), ElapsedMilliseconds]));
@@ -55,6 +95,14 @@ begin
   begin
     mmoLog.Lines.Add(Format('CRC32C SSE42_File: $%x；用时：%.5d 毫秒', [CRC32C_SSE42_File(strFileName), ElapsedMilliseconds]));
   end;
+end;
+
+procedure TForm1.srchbxFileInvokeSearch(Sender: TObject);
+begin
+  if not dlgOpen1.Execute() then
+    Exit;
+
+  srchbxFile.Text := dlgOpen1.FileName;
 end;
 
 end.
